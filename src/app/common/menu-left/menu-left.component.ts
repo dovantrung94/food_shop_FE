ceeaed30cfb873656b38de8ed40d6afb75e24874
Hoàng Yến <hoangyen4794@gmail.com>
2020-06-21 11:13:20 +0700

    Thêm thư viện + chỉnh giao diện menu-left

--- src/app/common/menu-left/menu-left.component.ts
+++ src/app/common/menu-left/menu-left.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../../service/home.service';
 import { User } from '../../model/user';
 import { Router } from '@angular/router';
+declare var $: any;
 
 @Component({
     selector: 'app-menu-left',
@@ -11,33 +12,79 @@ import { Router } from '@angular/router';
 export class MenuLeftComponent implements OnInit {
 
     user: User[];
+    detailUser = false;
     listData = [
         {
+            id : 1,
             link : '',
             tab : 'Dashboard',
             img: 'list.png'
         },
         {
+            id : 2,
             link : '',
             tab : 'Components',
-            img: 'list.png'
+            img: 'list.png',
+            child : [
+                {
+                    link : '',
+                    tab : 'Buttons',
+                    short: 'B'
+                },
+                {
+                    link : '',
+                    tab : 'Grid System',
+                    short: 'GS'
+                },
+                {
+                    link : '',
+                    tab : 'Panels',
+                    short: 'P'
+                }
+            ]
         },
         {
+            id : 3,
             link : '',
             tab : 'Form',
-            img: 'list.png'
+            img: 'list.png',
+            child : [
+              {
+                  link : '',
+                  tab : 'Regular Forms',
+                  short: 'RF'
+              },
+              {
+                  link : '',
+                  tab : 'Extended Forms',
+                  short: 'EF'
+              },
+              {
+                  link : '',
+                  tab : 'Validation Forms',
+                  short: 'VF'
+              },
+              {
+                  link : '',
+                  tab : 'Wizard',
+                  short: 'W'
+              }
+          ]
         },
         {
+            id : 4,
             link : '',
             tab : 'Tables',
             img: 'list.png'
         },
         {
+            id : 5,
             link : '',
             tab : 'Maps',
             img: 'list.png'
         },
         {
+            id : 6,
             link : '',
             tab : 'Charts',
             img: 'list.png'
@@ -52,6 +99,23 @@ export class MenuLeftComponent implements OnInit {
         localStorage.removeItem('userLogin');
 
         this.router.navigate(['login']);
+    }
+
+    showDetailTab (value) {
+        if (value.child) {
+            $("#listItem" + value.id).slideToggle("slow");
+            if (!value.show) {
+                value.show = false;
+            }
+            value.show = !value.show;
+        } else {
+            // go to new tab
+        }
+    }
+
+    showDetailUser () {
+        $('.box-infor').slideToggle("slow");
+        this.detailUser = !this.detailUser;
     }
 
 }
